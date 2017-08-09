aac281f8485fed22a34ead351c552b9aa3037cd5
马美英 <mamy@codoon.com>
2017-08-09 16:18:44 +0800

    update

---
+++ index.js
@@ -0,0 +1,10 @@
+export function ArrayRandomSort(arr) {
+    var newArray = [];
+    while (arr.length > 0) {
+        //随机获取原数组中的元素，存到新数组，并删除原数组该元素
+        var currIndex = Math.floor(Math.random() * arr.length);
+        newArray.push(arr[currIndex]);
+        arr.splice(currIndex, 1);
+    }
+    return newArray;
+}
